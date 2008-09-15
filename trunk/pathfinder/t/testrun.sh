a7bdb23b5ce5ddc7a5627a30185e09481d9ed91b
wrlach <wrlach@d03b4698-4336-0410-a006-270bfc9c5283>
2008-09-15 15:57:18 +0000

    Unit tests new use WvStream's test runner program, much easier to parse output.
    Needs extending to work with an out-of-tree WvStreams (not really useful
    at the moment, because there's no distributable version of the WvStreams
    required for Pathfinder).

---
+++ trunk/pathfinder/t/testrun.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# stupid script to execute WvStreams unit test stuff. right now only set up to 
+# run tests with in-tree version of wvstreams, but should be extendable to 
+# do so out of tree as well
+
+WVTESTHELPER="$WVSTREAMS_SRC/wvtestrunner.pl"
+SUPPRESSIONS="$WVSTREAMS_SRC/wvstreams.supp"
+VALGRIND="valgrind --tool=memcheck --leak-check=yes --num-callers=10 --log-file=valgrind.log --suppressions=$SUPPRESSIONS"
+
+$WVTESTHELPER $VALGRIND t/all.t $TESTNAME
